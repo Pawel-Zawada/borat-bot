809489b29748c8d39f4272503f87accf4cb82d57
Pawel Zawada <zawadapawelmail@gmail.com>
2021-10-17 15:01:35 +0200

    cleanup startup and shutdown code - DRY `discord.go`

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "nextrock/borat_bot/discord"
+import (
+	"log"
+	"nextrock/borat_bot/discord"
+	"os"
+	"os/signal"
+)
 
 func main() {
-	discord.Run()
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
+	go discord.Run()
+
+	// Keep running until received shutdown signal
+	sig := <-stop
+	discord.Stop()
+	log.Println("Stopping Go process", sig)
 }
